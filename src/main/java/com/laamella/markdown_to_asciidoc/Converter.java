db0613af344498911313d0a98031af450f557c3b
matozoid <hexagonaal@gmail.com>
2014-12-07 23:25:30 +0100

    Fix warnings

--- src/main/java/com/laamella/markdown_to_asciidoc/Converter.java
+++ src/main/java/com/laamella/markdown_to_asciidoc/Converter.java
@@ -3,45 +3,38 @@ package com.laamella.markdown_to_asciidoc;
 import org.pegdown.Extensions;
 import org.pegdown.PegDownProcessor;
 import org.pegdown.ast.RootNode;
+
 import java.io.*;
 
 public class Converter {
-
-    public static void main(String[] args) {
-        File input = null;
-        if (args.length == 0) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
             System.err.println("markdown_to_asciidoc: Please specify a file to convert");
             return;
         }
 
-        input = new File(args[0]);
-
+        File input = new File(args[0]);
         if (!input.exists()) {
             System.err.println("markdown_to_asciidoc: Cannot find the specified file to convert");
             return;
         }
 
-        BufferedReader reader = null;
         try {
-          reader = new BufferedReader(new FileReader(input));
-          StringBuffer buffer = new StringBuffer();
-          String line = null;
-          while ((line = reader.readLine()) != null) {
-            buffer.append(line + "\n");
-          }
-
-          reader.close();
-          System.out.println(convertMarkdownToAsciiDoc(buffer.toString().trim()));
-        } catch (IOException e) {
-          System.err.println("markdown_to_asciidoc: An error occurred while reading the input file");
-        }
-        finally {
-          if (reader != null) {
+            BufferedReader reader = new BufferedReader(new FileReader(input));
             try {
-              reader.close();
+                StringBuilder buffer = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+
+                reader.close();
+                System.out.println(convertMarkdownToAsciiDoc(buffer.toString().trim()));
+            } finally {
+                reader.close();
             }
-            catch (IOException e2) {}
-          }
+        } catch (IOException e) {
+            System.err.println("markdown_to_asciidoc: An error occurred while reading the input file");
         }
     }
 
