ff9a5340d0fe3e59ca66d51a38a4d125cf4dd99a
Dan Allen <dan.j.allen@gmail.com>
2014-12-07 03:33:49 -0700

    update Gradle build, add main class method, add distribution
    - update a Gradle build
    - turn Converter into a very rudimentary main class
    - package a distribution with a launcher (run ./gradlew distZip)
    - add switch to enable Maven local repository

--- src/main/java/com/laamella/markdown_to_asciidoc/Converter.java
+++ src/main/java/com/laamella/markdown_to_asciidoc/Converter.java
@@ -3,8 +3,47 @@ package com.laamella.markdown_to_asciidoc;
 import org.pegdown.Extensions;
 import org.pegdown.PegDownProcessor;
 import org.pegdown.ast.RootNode;
+import java.io.*;
 
 public class Converter {
+
+    public static void main(String[] args) {
+        File input = null;
+        if (args.length == 0) {
+            System.err.println("markdown_to_asciidoc: Please specify a file to convert");
+            return;
+        }
+
+        input = new File(args[0]);
+
+        if (!input.exists()) {
+            System.err.println("markdown_to_asciidoc: Cannot find the specified file to convert");
+            return;
+        }
+
+        BufferedReader reader = null;
+        try {
+          reader = new BufferedReader(new FileReader(input));
+          StringBuffer buffer = new StringBuffer();
+          String line = null;
+          while ((line = reader.readLine()) != null) {
+            buffer.append(line + "\n");
+          }
+
+          reader.close();
+          System.out.println(convertMarkdownToAsciiDoc(buffer.toString().trim()));
+        } catch (IOException e) {
+          System.err.println("markdown_to_asciidoc: An error occurred while reading the input file");
+        }
+        finally {
+          if (reader != null) {
+            try {
+              reader.close();
+            }
+            catch (IOException e2) {}
+          }
+        }
+    }
 
     public static String convertMarkdownToAsciiDoc(String markdown) {
         PegDownProcessor processor = new PegDownProcessor(Extensions.ALL);
