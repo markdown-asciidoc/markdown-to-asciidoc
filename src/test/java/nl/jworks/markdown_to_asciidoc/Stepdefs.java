7c9e082cdcb7b24733a863e689dfcf45d6edf103
EDGA6280 <david.gautier-ext@pole-emploi.fr>
2023-01-25 11:52:06 +0100

    feat(maven): convert to maven project with up to date dependencies

--- src/test/java/nl/jworks/markdown_to_asciidoc/Stepdefs.java
+++ src/test/java/nl/jworks/markdown_to_asciidoc/Stepdefs.java
@@ -1,8 +1,9 @@
 package nl.jworks.markdown_to_asciidoc;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.junit.Assert.assertEquals;
 
